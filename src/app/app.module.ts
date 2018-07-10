19e776ac678d0cf7e0abd2c2124eda0516d234ab
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2018-07-11 01:04:45 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './landing-page/header/header.component';
+import { FooterComponent } from './landing-page/footer/footer.component';
+import { Body1Component } from './landing-page/body1/body1.component';
+import { Body2Component } from './landing-page/body2/body2.component';
+import { Body3Component } from './landing-page/body3/body3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    Body1Component,
+    Body2Component,
+    Body3Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/landing-page/body3/body3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body3',
+  templateUrl: './body3.component.html',
+  styleUrls: ['./body3.component.less']
+})
+export class Body3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
